354f59ddd4f1ddf369c08109da45d5d71cfb2873
Juuniggi <juho.e.nurmi@edu.turkuamk.fi>
2023-03-10 09:43:24 +0200

    ICT Update

--- Assets/Menut/QuitGame.cs
+++ Assets/Menut/QuitGame.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuitGame : MonoBehaviour
 {
-    void doExitGame()
+    public void doExitGame()
     {
         Application.Quit();
     }
